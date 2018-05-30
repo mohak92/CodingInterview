439abcce0539e1b7d54fb6a0f14952cc761e1f58
mohaktamhane <mohaktamhane@192.168.1.10>
2018-05-30 10:18:04 -0400

    Coding Questions and Solutions for Coding Interview

---
+++ Cracking the Coding Interview/src/arraysAndStrings/URLify.java
@@ -0,0 +1,36 @@
+package arraysAndStrings;
+
+public class URLify {
+
+	void ReplaceSpaces(char[] str, int trueLength){
+		int spaceCount = 0, index, i = 0;
+		for(i = 0; i < trueLength; i++) {
+			if(str[i] == ' ') {
+			spaceCount++;
+			}
+		}
+		index = trueLength + spaceCount * 2;
+		if(trueLength < str.length) str[trueLength] = '\0';
+		for(i = trueLength - 1; i >= 0; i--) {
+			if(str[i] == ' ') {
+				str[index - 1] = '0';
+				str[index - 2] = '2';
+				str[index - 3] = '%';
+				index = index - 3;
+			}else {
+				str[index - 1] = str[i];
+				index--;
+			}
+		}
+		System.out.println(str);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		URLify url = new URLify();
+		char[] strArray = {'M', 'r', ' ', 'J', 'o', 'h', 'n', ' ', 'S', 'm', 'i', 't', 'h', ' ', ' ', ' ', ' '};
+		url.ReplaceSpaces(strArray, 13);
+
+	}
+
+}
